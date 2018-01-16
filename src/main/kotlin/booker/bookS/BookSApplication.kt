8d93c8fe3681038735543fff532baa5c4aa08bc8
baconadmiral <andrewwilliams1087@gmail.com>
2018-01-15 20:02:52 -0700

    Initial commit

---
+++ src/main/kotlin/booker/bookS/BookSApplication.kt
@@ -0,0 +1,11 @@
+package booker.bookS
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BookSApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BookSApplication::class.java, *args)
+}
